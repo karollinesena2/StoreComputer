c0e049b88b817398020e41f418d9933b08b6189d
Anna <karollinesena@hotmail.com>
2022-09-15 15:36:22 -0300

    add componentes

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+import { IProduto } from '../produtos';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto[] | undefined;
+
+  constructor(
+    private produtosService: ProdutosService
+    )  { }
+  
+
+  ngOnInit(): void {
+    this.produtos = this.produtosService.getAll();
+  }
+
+}
