4058a9e1daf7815389fc9900f2964a4e94468ace
Anna <karollinesena@hotmail.com>
2022-09-15 18:13:13 -0300

    add services

--- src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
+++ src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IProduto } from 'src/app/produtos';
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { CarrinhoComponent } from 'src/app/carrinho/carrinho.component';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
 import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
@@ -14,13 +17,25 @@ export class DetalhesProdutosComponent implements OnInit {
 
   constructor(
     private produtosService: ProdutosService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService
   ) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const produtoId =  Number(routeParams.get("id"));
     this.produto = this.produtosService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho() {
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho");
+    const produto: IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto);
+
   }
 
 }
